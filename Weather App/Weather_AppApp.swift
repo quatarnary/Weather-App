f0493e322f7da95fe4eb26772c2c45efed090ba3
Ilyas Aslan <152633183+quatarnary@users.noreply.github.com>
2024-05-22 17:19:12 +0300

    Initial Commit

---
+++ Weather App/Weather_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Weather_AppApp.swift
+//  Weather App
+//
+//  Created by Bugra Aslan on 22.05.2024.
+//
+
+import SwiftUI
+
+@main
+struct Weather_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
