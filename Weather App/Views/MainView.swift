720c5396b8c4887ef915700bfb87ec8337cb6683
Ilyas Aslan <152633183+quatarnary@users.noreply.github.com>
2024-05-30 15:34:49 +0300

    tabview and toolbar added
    tabview manages the LocationWeatherDataViews. toolbar has 3 items, one of them opens a sheet to show the map view, second one show the tabview page index, third one opens the Menu to manage the locations and the settings

--- Weather App/Views/MainView.swift
+++ Weather App/Views/MainView.swift
@@ -9,24 +9,69 @@ import SwiftUI
 
 struct MainView: View {
     @State var weather: WeatherResponse?
+    @State var selectionTabView: Int = 1
+    @State var isPresented = false
     
     var body: some View {
-        TabView {
-            LocationWeatherDataView(weather: weather)
-                .badge(1)
-                .tabItem {
-                    Label("Current Location", systemImage: "tray.and.arrow.down.fill")
+        NavigationStack {
+            TabView(selection: $selectionTabView) {
+                Text("Current Location Page")
+                    .badge("!")
+                    .tabItem {
+                        Label("Locations", systemImage: "tray.and.arrow.up.fill")
+                    }
+                    .tag(0)
+                LocationWeatherDataView(weather: weather)
+                    .badge(1)
+                    .tabItem {
+                        Label("Current Location", systemImage: "tray.and.arrow.down.fill")
+                    }
+                    .tag(1)
+                Text("My Locations 2nd Item Page")
+                    .badge("!")
+                    .tabItem {
+                        Label("Settings", systemImage: "person.crop.circle.fill")
+                    }
+                    .tag(2)
+            }
+            .tabViewStyle(.page(indexDisplayMode: .never))
+            .background(.ultraThinMaterial)
+            .background(LinearGradient(colors: [.green, .teal], startPoint: .bottom, endPoint: .top))
+            .toolbar {
+                ToolbarItem(placement: .bottomBar) {
+                    HStack(spacing: 20) {
+                        Button("??Map View??") {
+                            isPresented.toggle()
+                        }
+                        .sheet(isPresented: $isPresented, content: {
+                            Text("Map Page")
+                            Button("Close the sheet") {
+                                isPresented.toggle()
+                            }
+                        })
+                        Spacer()
+                        HStack {
+                            ForEach(0..<3) { index in
+                                Circle()
+                                    .frame(width: 8, height: 8)
+                                    .foregroundColor(index == self.selectionTabView ? .accentColor : .white)
+                                    .onTapGesture {
+                                        if index < self.selectionTabView {
+                                                self.selectionTabView -= 1
+                                            }
+                                        if index > self.selectionTabView {
+                                            self.selectionTabView += 1
+                                        }
+                                    }
+                            }
+                        }
+                        Spacer()
+                        NavigationLink("Settings") {
+                            WeatherSummaryView(weather: weather)
+                        }
+                    }
                 }
-            Text("My Locations Page")
-                .badge("!")
-                .tabItem {
-                    Label("Locations", systemImage: "tray.and.arrow.up.fill")
-                }
-            Text("Settings Page")
-                .badge("!")
-                .tabItem {
-                    Label("Settings", systemImage: "person.crop.circle.fill")
-                }
+            }
         }
     }
 }
