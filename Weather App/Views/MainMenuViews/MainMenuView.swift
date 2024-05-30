78958ab6882db2f9c91a748bc1a3db49e185965b
Ilyas Aslan <152633183+quatarnary@users.noreply.github.com>
2024-05-30 15:58:49 +0300

    change Text view with LocationCardView

--- Weather App/Views/MainMenuViews/MainMenuView.swift
+++ Weather App/Views/MainMenuViews/MainMenuView.swift
@@ -19,7 +19,7 @@ struct MainMenuView: View {
                 TextField("abc", text: $searchText)
                 // swift cries because we don't have id here, it can be solved as putting id: \.self, however the model for favorite locations will have the id variable as UUID so it will be solved when the time comes
                 ForEach(0..<favoriteLocationCount) { card in
-                    Text("Location Card")
+                    LocationCardView()
                 }
             }
             .toolbar {
