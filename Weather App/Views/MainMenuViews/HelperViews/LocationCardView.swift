07982cf62112086b982341bbf9b6e9527c878229
Ilyas Aslan <152633183+quatarnary@users.noreply.github.com>
2024-05-30 15:49:27 +0300

    create LocationCardView

---
+++ Weather App/Views/MainMenuViews/HelperViews/LocationCardView.swift
@@ -0,0 +1,18 @@
+//
+//  LocationCardView.swift
+//  Weather App
+//
+//  Created by Bugra Aslan on 30.05.2024.
+//
+
+import SwiftUI
+
+struct LocationCardView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LocationCardView()
+}
