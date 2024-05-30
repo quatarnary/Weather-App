4565d13c0b1c6ec762f88a50c01e1f4f155f19a7
Ilyas Aslan <152633183+quatarnary@users.noreply.github.com>
2024-05-30 15:48:40 +0300

    draft of MainMenuView

--- Weather App/Views/MainMenuViews/MainMenuView.swift
+++ Weather App/Views/MainMenuViews/MainMenuView.swift
@@ -8,8 +8,28 @@
 import SwiftUI
 
 struct MainMenuView: View {
+    @State var searchText: String = ""
+    let favoriteLocationCount: Int = 3
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationStack {
+            VStack {
+                Text("Weather")
+                    .font(.largeTitle)
+                TextField("abc", text: $searchText)
+                // swift cries because we don't have id here, it can be solved as putting id: \.self, however the model for favorite locations will have the id variable as UUID so it will be solved when the time comes
+                ForEach(0..<favoriteLocationCount) { card in
+                    Text("Location Card")
+                }
+            }
+            .toolbar {
+                ToolbarItem(placement: .topBarTrailing) {
+                    NavigationLink("Settings") {
+                        Text("abc")
+                    }
+                }
+            }
+        }
     }
 }
 
