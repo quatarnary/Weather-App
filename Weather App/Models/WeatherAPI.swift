174f0a47a0aabc297ae8f01704ae34e8cb902e0b
Ilyas Aslan <152633183+quatarnary@users.noreply.github.com>
2024-06-02 16:30:07 +0300

    change build-setting, API-Key url based on debug version
    I assume xcsettings file generated because I set the #if DEBUG logic into the code. But I'm not sure, I tried to find it on google however there was no pin-point answer. However based on people questions, I come-up with this answer 😸

--- Weather App/Models/WeatherAPI.swift
+++ Weather App/Models/WeatherAPI.swift
@@ -9,11 +9,25 @@ import Foundation
 
 // MARK: - APIKEY
 struct WeatherAPI {
+    
+    #if DEBUG
+    static var keyURL = Bundle.main.url(forResource: "local", withExtension: "plist")
+    #else
     static var keyURL = Bundle.main.url(forResource: "API-Keys", withExtension: "plist")
+    #endif
 
-    static let url = URL(string: "https://api.weatherapi.com/v1/forecast.json?key=\(getKey())&q=istanbul&days=1&aqi=yes&alerts=no")!
+    static let url = URL(string: "https://api.weatherapi.com/v1/forecast.json?key=\(getKey())&q=istanbul&days=3&aqi=yes&alerts=no")!
     
     static func getKey() -> String {
         return NSDictionary(contentsOf: keyURL!)!["WEATHER_API_KEY"] as! String
     }
 }
+
+
+/*
+ static let url = URL(string: "https://api.weatherapi.com/v1/forecast.json?key=\(getKey("WEATHER_API_KEY"))&q=istanbul&days=3&aqi=yes&alerts=no")!
+ 
+ static func getKey(_ keyName: String) -> String {
+ return NSDictionary(contentsOf: keyURL!)![keyName] as! String
+ }
+ */
