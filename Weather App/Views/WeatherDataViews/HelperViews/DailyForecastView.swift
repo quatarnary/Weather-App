c028e8a45cc39778afb705139bdcaee18cb8d642
Ilyas Aslan <152633183+quatarnary@users.noreply.github.com>
2024-07-01 01:50:33 +0300

    implement a check for whether or not we have the forecastday value

--- Weather App/Views/WeatherDataViews/HelperViews/DailyForecastView.swift
+++ Weather App/Views/WeatherDataViews/HelperViews/DailyForecastView.swift
@@ -21,19 +21,23 @@ struct DailyForecastView: View {
     var body: some View {
         VStack(alignment: .leading) {
             Text("\(forecastDayCount) day forecast")
-            ForEach((weather.forecast?.forecastday)!) { forecastday in
-                let day = dateFormatter.string(from: Date(timeIntervalSince1970: TimeInterval(forecastday.dateEpoch!)))
-                let today = dateFormatter.string(from: Date())
-                HStack {
-                    Text("\(day == today ? "Today" : day)")
-                        .frame(minWidth: 100, alignment: .leading)
-                    Spacer()
-                    Image(systemName: "sun.min")
-                    Spacer()
-                    Text("L: \(forecastday.day?.mintempC ?? -999, specifier: "%.1f")°")
-                    Spacer()
-                    Text("H: \(forecastday.day?.maxtempC ?? -999, specifier: "%.1f")°")
-                    Spacer()
+            // if the forecastday.isEmpty is nil we are assuming it is empty
+            // if the forecastday is not empty OR if the forecastday is not nil
+            if !(weather.forecast?.forecastday?.isEmpty ?? true) {
+                ForEach((weather.forecast?.forecastday)!) { forecastday in
+                    let day = dateFormatter.string(from: Date(timeIntervalSince1970: TimeInterval(forecastday.dateEpoch!)))
+                    let today = dateFormatter.string(from: Date())
+                    HStack {
+                        Text("\(day == today ? "Today" : day)")
+                            .frame(minWidth: 100, alignment: .leading)
+                        Spacer()
+                        Image(systemName: "sun.min")
+                        Spacer()
+                        Text("L: \(forecastday.day?.mintempC ?? -999, specifier: "%.1f")°")
+                        Spacer()
+                        Text("H: \(forecastday.day?.maxtempC ?? -999, specifier: "%.1f")°")
+                        Spacer()
+                    }
                 }
             }
         }
